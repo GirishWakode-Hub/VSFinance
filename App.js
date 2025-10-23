f1fd11209a36d9a04a4adffd1c3e4c146b65e28d
GirishWakode-Hub <girishwakode9@gmail.com>
2025-10-23 12:36:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Security from './components/Security';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
+import FinanceTypes from './components/FinanceTypes';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Services />
+              <Security />
+              <CTA />
+            </>
+          } />
+          <Route path="/finance-types" element={<FinanceTypes />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
